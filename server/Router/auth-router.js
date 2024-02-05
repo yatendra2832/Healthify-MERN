5e97db07a3730346d3edde5f869c89e373043de2
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-02-05 21:43:38 +0530

    add the schema validation to the route of the signup

--- server/Router/auth-router.js
+++ server/Router/auth-router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../Controller/auth-controller')
+const {signupSchema} = require('../validators/auth-validator')
+const validate = require('../Middleware/validate-middleware')
 
-router.route('/signup').post(authController.signup)
+router.route('/signup').post(validate(signupSchema), authController.signup)
 router.route('/login').post(authController.login)
 
 module.exports = router;
