25a7f4d4a6234de9d9f390856e5cd191ba7e3a57
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-02-05 21:42:37 +0530

    created the zod validation for the register

---
+++ server/validators/auth-validator.js
@@ -0,0 +1,29 @@
+const { z } = require('zod');
+
+const signupSchema = z.object({
+    username: z
+        .string({ required_error: "Name is Required" })
+        .trim()
+        .min(3, { message: "Name must be atleast of 3 characters" })
+        .max(255, { message: "Name must not be more that 255 characters" }),
+    email: z
+        .string({ required_error: "Email is Required" })
+        .trim()
+        .email({ message: "Invalid Email Address" })
+        .min(7, { message: "Email must be atleast of 7 characters" })
+        .max(255, { message: "Name must not be more that 255 characters" }),
+
+    phone: z
+        .string({ required_error: "Phone is Required" })
+        .trim()
+        .min(10, { message: "Phone must be atleast of 10 characters" })
+        .max(12, { message: "Phone must not be more that 12 characters" }),
+    password: z
+        .string({ required_error: "Password is Required" })
+        .trim()
+        .min(10, { message: "Password must be atleast of 10 characters" })
+        .max(12, { message: "Password must not be more that 12 characters" }),
+
+})
+
+module.exports = { signupSchema }
