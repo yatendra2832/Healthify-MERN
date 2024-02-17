0552de13e6d09998bc8f097c4396a02371a7d6df
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-02-17 12:49:13 +0530

    Tests Card for the test and scans

---
+++ client/src/components/Card/TestsCard.jsx
@@ -0,0 +1,34 @@
+// It is for the xray page of the second navbar
+import React from "react";
+
+const TestsCard = ({
+  title,
+  content,
+  buttonText,
+  imgSrc,
+  altText,
+  linkHref,
+}) => {
+  return (
+    <div class="card bg-primary text-white m-3 rounded-4 p-4 shadow">
+      <div class="container">
+        <h1 class="text-center my-4 fw-bold">
+          The Excellence of <span className="text-warning">{title}</span>
+        </h1>
+        <div class="row justify-content-center align-items-center">
+          <div class="col-12 col-md-4 mb-4">
+            <a href={linkHref}>
+              <img src={imgSrc} alt={altText} class="img-fluid rounded w-100" />
+            </a>
+          </div>
+          <div class=" col-12 col-md-8 ">
+            <p class="fw-semibold ">{content}</p>
+            <button class="btn btn-danger m-4 ">{buttonText}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestsCard;
