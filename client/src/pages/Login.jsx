674253d9cb4330069a1a79d90ebf7d53045bf557
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-02-21 08:07:25 +0530

    updated the login and signup images with the carousel

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,63 +1,113 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Login = () => {
+  const slides = [
+    {
+      image: "/images/Login_Signup/boxicon.png",
+      text: " Real-Time Packages",
+      heading: "Customized",
+    },
+    {
+      image: "/images/Login_Signup/complete.png",
+      text: " Honest Price Guaranteed",
+      heading: "Cost-Effective",
+    },
+    {
+      image: "/images/Login_Signup/Convenient.png",
+      text: " Free Home Sample Collection",
+      heading: "Convenient",
+    },
+    {
+      image: "/images/Login_Signup/cost-effective.png",
+      text: " Accurate Results On Time",
+      heading: "Carefree",
+    },
+    {
+      image: "/images/Login_Signup/timer.png",
+      text: " Proactive Wellness Report",
+      heading: "Complete",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
-    <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative my-4 mx-4">
-    <div className="col d-none d-lg-block m-4 p-4">
-      <img
-        src="/images/blue_images/login.png"
-        alt="contact image"
-        width={"400"}
-        height={"400"}
-        className=" rounded-circle color-primary shadow-lg"
-      />
-    </div>
-    <div className="col p-4 d-flex flex-column position-static">
-      <div className="container mt-1 ">
-        <h2 className="text-center text-primary fw-bold fs-1">Login</h2>
-        <form >
-          <div className="mb-4">
-            <label htmlFor="email" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              name="email"
-              className="form-control"
-              id="email"
-              placeholder="name@example.com"
-              required
-              autoComplete="off"
+    <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative m-4 p-4">
+      <div className="col col-md-6 my-2">
+        <Slider {...settings}>
+          {slides.map((slide, index) => (
+            <div key={index} className="text-center">
+              <div className="d-flex flex-column align-items-center ">
+                <img
+                  src={slide.image}
+                  alt={`login image ${index + 1}`}
+                  width={200}
+                  height={200}
+                  className="rounded-circle shadow"
+                />
+                <h2 className="my-2">{slide.heading}</h2>
+                <p className="mt-1">{slide.text}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <div className="col col-md-6 p-4 d-flex flex-column position-static">
+        <div className="container mt-1 ">
+          <h2 className="text-center text-primary fw-bold fs-1">Login</h2>
+          <form>
+            <div className="mb-4">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                id="email"
+                placeholder="name@example.com"
+                required
+                autoComplete="off"
+              />
+            </div>
 
-            />
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              id="password"
-              placeholder="Your Password"
-              required
-              autoComplete="off"
-              
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn btn-primary w-75  d-block mx-auto "
-          >
-            Submit
-          </button>
-        </form>
+            <div className="mb-4">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                id="password"
+                placeholder="Your Password"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary w-75  d-block mx-auto "
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,18 +1,69 @@
 import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Signup = () => {
+  const slides = [
+    {
+      image: "/images/Login_Signup/boxicon.png",
+      text: " Real-Time Packages",
+      heading: "Customized",
+    },
+    {
+      image: "/images/Login_Signup/complete.png",
+      text: " Honest Price Guaranteed",
+      heading: "Cost-Effective",
+    },
+    {
+      image: "/images/Login_Signup/Convenient.png",
+      text: " Free Home Sample Collection",
+      heading: "Convenient",
+    },
+    {
+      image: "/images/Login_Signup/cost-effective.png",
+      text: " Accurate Results On Time",
+      heading: "Carefree",
+    },
+    {
+      image: "/images/Login_Signup/timer.png",
+      text: " Proactive Wellness Report",
+      heading: "Complete",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
   return (
-    <div className="row g-0  rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative my-4 mx-4">
-      <div className="col d-none d-lg-block m-4 p-4">
-        <img
-          src="/images/blue_images/register.png"
-          alt="contact image"
-          width={"400"}
-          height={"400"}
-          className=" rounded-circle color-primary shadow-lg"
-        />
+    <div className="row g-0  rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative m-4 p-4">
+      <div className="col col-md-6 my-5">
+        <Slider {...settings}>
+          {slides.map((slide, index) => (
+            <div key={index} className="text-center">
+              <div className="d-flex flex-column align-items-center ">
+                <img
+                  src={slide.image}
+                  alt={`login image ${index + 1}`}
+                  width={200}
+                  height={200}
+                  className="rounded-circle shadow"
+                />
+                <h2 className="my-2">{slide.heading}</h2>
+                <p className="mt-1">{slide.text}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-      <div className="col p-4 d-flex flex-column position-static">
+      <div className="col col-md-6 p-4 d-flex flex-column position-static">
         <div className="container mt-1 ">
           <h2 className="text-center text-primary fw-bold fs-1">Signup</h2>
           <form>
