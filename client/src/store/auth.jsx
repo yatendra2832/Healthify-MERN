6d312cc8d75f064c21ee1e7cfbc28924aba0690e
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-02-29 11:50:50 +0530

    added authentication logic for access user data

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,23 +1,60 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState("");
+
   const storeTokenInLS = (serverToken) => {
+    setToken(serverToken);
     return localStorage.setItem("token", serverToken);
   };
 
+  let isLoggedIn = !!token;
+
+  // Logout
+  const LogoutUser = () => {
+    setToken("");
+    return localStorage.removeItem("token");
+  };
+
+  // User Authentication - to get the currently login data
+
+  const userAuthentication = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/user", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data.userData);
+      }
+    } catch (error) {
+      console.log("Error in Authentication: ", error);
+    }
+  };
+
+  useEffect(() => {
+    userAuthentication();
+  }, []);
+
   return (
-    <AuthContext.Provider value={storeTokenInLS}>
+    <AuthContext.Provider
+      value={{ isLoggedIn, LogoutUser, storeTokenInLS, user }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
 
 export const useAuth = () => {
-    const authContextValue = useContext(AuthContext);
-    if(!authContextValue){
-        throw new Error("useAuth used outside of the provider");
-    }
-    return authContextValue
+  const authContextValue = useContext(AuthContext);
+  if (!authContextValue) {
+    throw new Error("useAuth used outside of the provider");
+  }
+  return authContextValue;
 };
