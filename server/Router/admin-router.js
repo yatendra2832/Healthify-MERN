67aa3e8503c76811d9372ca59a3cbc780efb8f01
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-02 09:47:22 +0530

    adding authentication on the admin routes

--- server/Router/admin-router.js
+++ server/Router/admin-router.js
@@ -1,13 +1,14 @@
 const express =require('express');
 const router = express.Router();
 const adminController = require('../Controller/admin-controller')
+const authMiddleware = require("../Middleware/auth-middleware")
 
-router.route("/users").get(adminController.getAllUsers);
-
-
-router.route('/contacts').get(adminController.getAllContacts)
+router.route("/users").get(authMiddleware,adminController.getAllUsers);
 
 
-router.route('/appointments').get(adminController.getAllAppointments)
+router.route('/contacts').get(authMiddleware,adminController.getAllContacts)
+
+
+router.route('/appointments').get(authMiddleware,adminController.getAllAppointments)
 
 module.exports= router;
