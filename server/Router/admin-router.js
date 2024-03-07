0828e1c739a2c5df1ac6ba0ef23b9651cfa4885f
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-07 10:00:18 +0530

    routes for deleting the user contacts and appointments

--- server/Router/admin-router.js
+++ server/Router/admin-router.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const adminController = require('../Controller/admin-controller')
 const authMiddleware = require("../Middleware/auth-middleware")
 const adminMiddleware = require("../Middleware/admin-middleware");
+
+//Users Routes 
+// getting all users
 router.route("/users").get(authMiddleware, adminMiddleware, adminController.getAllUsers);
 
+// Deleting a Particular user
+router.route('/users/delete/:id').delete(authMiddleware, adminMiddleware, adminController.deleteUserById)
 
+
+// Contact Route
 router.route('/contacts').get(authMiddleware, adminMiddleware, adminController.getAllContacts)
-
+// Deleting a Particular user
+router.route('/contacts/delete/:id').delete(authMiddleware, adminMiddleware, adminController.deleteContactById)
 
 router.route('/appointments').get(authMiddleware, adminMiddleware, adminController.getAllAppointments)
+router.route('/appointments/delete/:id').get(authMiddleware, adminMiddleware, adminController.deleteAppointmentById)
 
 module.exports = router;
