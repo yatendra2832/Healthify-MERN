cfe601dcf5683971aa0472679ecee0abfd683cd2
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-05-18 18:30:39 +0530

    Enhance the ui of genetic testing

--- client/src/pages/GeneticTesting.jsx
+++ client/src/pages/GeneticTesting.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TestsCard from "../components/Card/TestsCard";
+import "../css/howitwork.css";
 
 import { GiDrippingTube } from "react-icons/gi";
 import { IoMdAnalytics } from "react-icons/io";
@@ -18,52 +19,56 @@ const GeneticTesting = () => {
         imgSrc="/images/TestImage/dexascan.jpg"
         altText="Labs"
         linkHref="#"
-        id={"6647a0a4dcf995ff27f01da1"}
+        id={"66489cdbdcf995ff27e1974e"}
       />
 
-      <div className="container my-5 bg-primary p-4 rounded-4">
-        <h1 className="text-light text-center mb-4 fst-italic">How it Works</h1>
+      <div className="howitworkcontainer my-5 bg-primary p-4 rounded-4 m-4">
+        <h1 className=" text-center  fw-bold mb-4 fst-italic ">
+          <span className="border-bottom border-3 rounded-5 px-4">
+            How it Works
+          </span>
+        </h1>
         <div className="d-flex justify-content-center align-items-center flex-wrap">
-          <div className="d-flex flex-column align-items-center mx-2">
-            <div className="card text-center p-3 shadow-lg h-100 d-flex flex-column align-items-center">
-              <GiDrippingTube className="icon fs-1 text-primary m-3 m-3" />
+          <div className="step-card mx-2">
+            <div className="card-content">
+              <GiDrippingTube className="icon fs-1 text-primary m-3" />
               <h5 className="card-title mt-auto text-warning my-4">
                 Sample Collection
               </h5>
             </div>
           </div>
-          <FaArrowRight className="text-warning fs-2 mx-2" />
-          <div className="d-flex flex-column align-items-center mx-2">
-            <div className="card text-center p-3 shadow-lg h-100 d-flex flex-column align-items-center">
+          <FaArrowRight className="text-warning fs-2 mx-2 arrow-icon" />
+          <div className="step-card mx-2">
+            <div className="card-content">
               <IoMdAnalytics className="icon fs-1 text-primary m-3" />
               <h5 className="card-title mt-auto text-warning my-4">
                 DNA Extraction
               </h5>
             </div>
           </div>
-          <FaArrowRight className="text-warning fs-2 mx-2" />
-          <div className="d-flex flex-column align-items-center mx-2">
-            <div className="card text-center p-3 shadow-lg h-100 d-flex flex-column align-items-center">
+          <FaArrowRight className="text-warning fs-2 mx-2 arrow-icon" />
+          <div className="step-card mx-2">
+            <div className="card-content">
               <TbReport className="icon fs-1 text-primary m-3" />
               <h5 className="card-title mt-auto text-warning my-4">
                 DNA Analysis
               </h5>
             </div>
           </div>
-          <FaArrowRight className="text-warning fs-2 mx-2" />
-          <div className="d-flex flex-column align-items-center mx-2">
-            <div className="card text-center p-3 shadow-lg h-100 d-flex flex-column align-items-center">
+          <FaArrowRight className="text-warning fs-2 mx-2 arrow-icon" />
+          <div className="step-card mx-2">
+            <div className="card-content">
               <FaUserDoctor className="icon fs-1 text-primary m-3" />
               <h5 className="card-title mt-auto text-warning my-4">
                 Report Generation
               </h5>
             </div>
           </div>
-          <FaArrowRight className="text-warning fs-2 mx-2" />
-          <div className="d-flex flex-column align-items-center mx-2 ">
-            <div className="card text-center p-3 shadow-lg h-100 d-flex flex-column align-items-center">
+          <FaArrowRight className="text-warning fs-2 mx-2 arrow-icon" />
+          <div className="step-card mx-2">
+            <div className="card-content">
               <LuFileSearch className="icon fs-1 text-primary m-3" />
-              <h5 className="card-title mt-auto text-warning my-4 ">
+              <h5 className="card-title mt-auto text-warning my-4">
                 Counseling By Expert
               </h5>
             </div>
