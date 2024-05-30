586b5c908cb90bf001bbbdba1f7a4a44ed63730b
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-05-30 22:56:17 +0530

    Implmented the card context

---
+++ client/src/store/CartContext.jsx
@@ -0,0 +1,50 @@
+import React, { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems((prevItems) => {
+      const existingItem = prevItems.find((cartItem) => cartItem._id === item._id);
+      if (existingItem) {
+        return prevItems.map((cartItem) =>
+          cartItem._id === item._id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+        );
+      } else {
+        return [...prevItems, { ...item, quantity: 1 }];
+      }
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems(cartItems.filter((item) => item._id !== id));
+  };
+
+  const incrementQuantity = (id) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item._id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decrementQuantity = (id) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item._id === id && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, incrementQuantity, decrementQuantity }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
